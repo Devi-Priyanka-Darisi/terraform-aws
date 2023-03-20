f657eef8955da75805d093f07a1d69fe29b83b5e
Devi-Priyanka-Darisi <darisidevi.tech@gmail.com>
2023-03-20 14:49:15 +0530

    subnet with public ip

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,12 @@ resource "aws_vpc" "lms--vpc" {
   }
 }
 
+# subnet -- with enabling public-ip
+resource "aws_subnet" "lms--subnet" {
+  vpc_id                  = aws_vpc.lms--vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "lms-subnet"
+  }
+}
